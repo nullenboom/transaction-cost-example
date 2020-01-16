ced0fd5695789a70e2f06f02f6e80762714d7374
Niklas Ullenboom <niklas.ullenboom@paluno.uni-due.de>
2020-01-16 17:55:08 +0100

    adds dir for web3 and eth gas reporter

---
+++ eth-gas-reporter/truffle-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  },
+    mocha: {
+		reporter: "eth-gas-reporter",
+		reporterOptions: {
+			currency: "eur",
+			excludeContracts: ["Migrations"],
+			onlyCalledMethods: false,
+		}
+	}
+};
